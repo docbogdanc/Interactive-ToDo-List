dc1d0531887cd61c99546d8f5b69fa732d8307d3
docbogdanc <docbogdanc@yahoo.com>
2024-04-27 10:34:55 +0100

    form for ranking created , need submit

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,44 @@
 const buttonAddTask = document.getElementById('addTask');
 const tableElem = document.getElementById('table');
 const tableBody = document.getElementById('tableBody')
+let ranking, task, action
 
 buttonAddTask.addEventListener ('click', function() {
     console.log("button pressed");
+    const newRaw = document.createElement('tr');
+    const formElem = document.createElement('form');
+    const inputElem = document.createElement('input')
+    const formDataRanking = document.createElement('td');
+    inputElem.setAttribute('type', 'text');
+    inputElem.setAttribute('placeholder', 'Enter Ranking');
+    formDataRanking.appendChild(inputElem)
+    const formTask = document.createElement('td');
+    formTask.setAttribute('type', 'text');
+    const formAction = document.createElement('td');
+    formAction.setAttribute('type', 'text');
 
+    formDataRanking.appendChild(formElem);
+
+
+    
+    newRaw.appendChild(formDataRanking)
+    tableBody.appendChild(newRaw);
 });
 
-function addTask () {
+function addTask (ranking, task, action) {
     const newTaskElem = document.createElement('tr');
     const newTaskElementData1 = document.createElement('td');
     newTaskElementData1.textContent ="test";
-    // const newTaskElementData2 = document.createElement('td');
-    // const newTaskElementData3 = document.createElement('td');
+    const newTaskElementData2 = document.createElement('td');
+    newTaskElementData2.textContent ="test2";
+    const newTaskElementData3 = document.createElement('td');
+    newTaskElementData3.textContent ="test3";
+    //  add the data to the table raw
     newTaskElem.appendChild(newTaskElementData1);
+    newTaskElem.appendChild(newTaskElementData2);
+    newTaskElem.appendChild(newTaskElementData3);
 
-    
+    // add the raw to the table 
     // tableElem.insertBefore(newTaskElem, tableElem.children[1]);
     tableBody.appendChild(newTaskElem);
 }
